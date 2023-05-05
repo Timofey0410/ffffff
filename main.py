9252484cec357312a770abf3b40155fe0be54d17
Timofey0410 <131515731+Timofey0410@users.noreply.github.com>
2023-05-05 16:13:15 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+a = 10 
+b = 20 
+
+znak = "+"
+if znak == "+" : 
+    c = a + b 
+    print(c) 
+    
+a = 20 
+b = 10 
+
+znak = "-" 
+if znak == "-" :
+    c = a - b 
+    print(c)
+    
+a = 10 
+b = 20
+
+znak = "*"
+if znak == "*" : 
+    c = a * b 
+    print(c)
+
+a = 20 
+b = 10 
+
+znak = "/"
+if znak == "/" :
+    c = a / b 
+    print(c)
+
+
+a = 5 
+b = 3 
+p = (a + b) * 2 
+print("периметр = " ,p)
+s = a * b 
+print("площадь = " ,s)
+
+a = 5 
+p = a * 4 
+print("периметр = " ,p)
+s = a ** 2
+print("периметр = " ,s)
+
